cf69e643533e70f0315f36a25cb36a6e07f0a4da
Banuchandar <105719358+BhanuChandar-AWS@users.noreply.github.com>
2025-09-03 12:36:00 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,9 +6,10 @@ window._config = {
       
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://zqz865k7t3.execute-api.eu-north-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
