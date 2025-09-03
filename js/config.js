17774c59d6b6e6184b2e878076c7b0422cc6a3bc
Banuchandar <105719358+BhanuChandar-AWS@users.noreply.github.com>
2025-09-03 11:51:21 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,12 +3,13 @@ window._config = {
         userPoolId: 'eu-north-1_4KYnwjdcT', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'tnn2oofqse5rlmmr8404p0n79', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
-        username = "bhanu51aprdc@gmail.com"
+      
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
