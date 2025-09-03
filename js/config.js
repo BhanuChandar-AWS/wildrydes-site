d1891f32098af240d0dd3d84de86aaeb6790fc0d
Banuchandar <105719358+BhanuChandar-AWS@users.noreply.github.com>
2025-09-03 11:22:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_4KYnwjdcT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2c3s1lht43khr9l0jkkcddgfag', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '168smvq8kjaec861r5rktqjicv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
